76ccfcd967d2e1b1ddd94c23cee023f01775c8ec
Tu Profedog <tuprofedog@gmail.com>
2025-09-10 08:07:46 -0600

    feat: added navbar

--- src/app/landing-page/components/navbar/navbar.ts
+++ src/app/landing-page/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  constructor(private _authService: AuthService) {}
 
+  protected closeSession() {
+    this._authService.closeSession();
+  }
 }
