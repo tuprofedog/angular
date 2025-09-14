53626d55bc0d4b59840e82d2decb1a21410301b5
Tu Profedog <tuprofedog@gmail.com>
2025-09-14 09:23:59 -0600

    feat: added ssr route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'home', renderMode: RenderMode.Client },
   {
     path: '**',
     renderMode: RenderMode.Prerender
